e45a4f9eb48720876d0ea2046079db8a854c63b6
DiogenesPuig <diogenespuig@gmail.com>
2020-08-19 17:22:11 -0300

    models were made and added to the admin site

--- Biblioteca/biblioteca/admin.py
+++ Biblioteca/biblioteca/admin.py
@@ -3,3 +3,11 @@ from biblioteca.models import *
 
 # Register your models here.
 
+
+admin.site.register(Material,)
+admin.site.register(Libro,)
+admin.site.register(Revista,)
+admin.site.register(Persona,)
+admin.site.register(Alumno,)
+admin.site.register(Profesor,)
+admin.site.register(Prestamo,)
--- Biblioteca/biblioteca/models.py
+++ Biblioteca/biblioteca/models.py
@@ -2,3 +2,39 @@ from django.db import models
 
 # Create your models here.
 
+class Material(models.Model):
+    tipoMaterial = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=50)
+    titulo = models.CharField(max_length=50)
+    anio = models.IntegerField()
+    status = models.CharField(max_length=20)
+
+class Libro(Material):
+    editorial = models.CharField(max_length=100)
+
+class Revista(Material):
+    nombre = models.CharField(max_length=100)
+
+class Persona(models.Model):
+    tipoPersona = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=50)
+    correo = models.CharField(max_length=50)
+    telefono = models.IntegerField()
+    numLibros = models.IntegerField()
+    adeudo = models.IntegerField()
+
+class Alumno(Persona):
+    matricula = models.IntegerField()
+
+class Profesor(Persona):
+    numEmpleado = models.IntegerField()
+
+class Prestamo(models.Model):
+    codigo = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+    fechaSalida = models.DateField()
+    fechaRegreso = models.DateField()
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
